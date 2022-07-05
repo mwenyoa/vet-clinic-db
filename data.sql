660a69e2df6c74b6673dd09ed716e221db3482ba
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-05 22:51:57 +0200

    Update Insert data query

--- data.sql
+++ data.sql
@@ -1,10 +1,6 @@
 /* Populate database with sample data. */
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES('Agumon', 'Feb-03-2020', 0, true,  10.23),
-      ('Gabumon', 'Nov-15-2018', 2, true,  8),
-      ('Pikachu', 'Jan-7-2021', 1, false,  15.04),
-      ('Devimon', 'May-12-2017', 5, true,  11),
-      ('Charmander', 'Feb-08-2020', 0, false, 11), 
+VALUES('Charmander', 'Feb-08-2020', 0, false, 11), 
       ('Plantmon', 'Nov-15-2021', 2, true, 5.7),
       ('Squirtle', 'Apr-02-1993', 3, false, 12.13),
       ('Angemon', 'Jan-12-2005', 1, true, 45),
