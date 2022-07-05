dc7bfcfd185e30e992e12ddd4ba84f4f156f74b6
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-05 22:18:05 +0200

    Add Insert data query syntax

--- data.sql
+++ data.sql
@@ -3,5 +3,12 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Agumon', 'Feb-03-2020', 0, true,  10.23),
       ('Gabumon', 'Nov-15-2018', 2, true,  8),
       ('Pikachu', 'Jan-7-2021', 1, false,  15.04),
-      ('Devimon', 'May-12-2017', 5, true,  11);
+      ('Devimon', 'May-12-2017', 5, true,  11),
+      ('Charmander', 'Feb-08-2020', 0, false, 11), 
+      ('Plantmon', 'Nov-15-2021', 2, true, 5.7),
+      ('Squirtle', 'Apr-02-1993', 3, false, 12.13),
+      ('Angemon', 'Jan-12-2005', 1, true, 45),
+      ('Boarmon', 'Jun-7-2005', 7, true, 20.4),
+      ('Blossom', 'Oct-13-1998', 3, true, 17),
+      ('Ditto', 'May-14-2022', 4, true, 22);
      
