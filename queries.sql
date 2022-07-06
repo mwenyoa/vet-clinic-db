f871cab460f86ceb4891b2fb6ea1d5b049db1051
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-06 18:54:02 +0200

    Update the max and min animal weight query to meet requirement

--- queries.sql
+++ queries.sql
@@ -49,8 +49,8 @@ MAX(escape_attempts) as Max_Escape_Attempts
 FROM animals GROUP BY neutered,  
 Animal_Escaping_The_Most ORDER BY Max_Escape_Attempts DESC LIMIT 1;
 /* What is the minimum and maximum weight of each type of animal? */
-SELECT MAX(weight_kg) as ANIMAL_MAX_WEIGHT,
-MIN(weight_kg) as ANIMAL_MIN_WEIGHT FROM animals;
+SELECT  species, MAX(weight_kg) as ANIMAL_MAX_WEIGHT,
+MIN(weight_kg) as ANIMAL_MIN_WEIGHT FROM animals GROUP BY species;
 /* */
 SELECT species, AVG(escape_attempts) as AVERAGE_ESCAPE_ATTEMPTS  FROM animals
 WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31' GROUP BY species;
