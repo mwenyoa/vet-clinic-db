d8e31dda3cb308d27bf2c5d16f917455d844e90f
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-06 20:38:28 +0200

    Add foreign key to animals table from owners table

--- schema.sql
+++ schema.sql
@@ -29,3 +29,9 @@ CREATE TABLE species(
  ALTER TABLE animals
  ADD CONSTRAINT species_id
  FOREIGN KEY(species_id) REFERENCES species(id);
+
+ /*Add foreign key to animals table from owners table*/
+ ALTER TABLE animals ADD  owners_id INT;
+ ALTER TABLE animals 
+ ADD CONSTRAINT owners_id
+ FOREIGN KEY(owners_id) REFERENCES owners(id);
