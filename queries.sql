c35140414e30dd16384c5e11465aca312f19bd2c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-06 13:20:21 +0200

    Add fourth transaction to set animal weight, create savepoit and commit

--- queries.sql
+++ queries.sql
@@ -27,3 +27,13 @@ BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+/* */
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT mydata;
+UPDATE animals SET weight_kg = weight_kg * (-1);
+ROLLBACK TO mydata;
+UPDATE animals SET weight_kg = weight_kg * (-1) 
+WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
