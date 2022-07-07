2184842c073b2bcb55c5837c61e5600c68a32cca
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-07-07 05:41:42 +0200

    Add query to set animal  species toDigimon based on foreign key

--- data.sql
+++ data.sql
@@ -23,3 +23,10 @@ INSERT INTO species(name)
 VALUES('Pokemon'), ('Digimon');
 
 SELECT * FROM species;
+
+/* Modify your inserted animals so it includes the species_id value:
+If the name ends in "mon" it will be Digimon */
+UPDATE animals SET species_id = (
+SELECT id FROM species WHERE name ='Digimon') 
+WHERE name LIKE '%mon';
+SELECT * FROM animals;
